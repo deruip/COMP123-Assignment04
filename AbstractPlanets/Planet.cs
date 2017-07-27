70d3da557d1154a96711dbc73e626b738ca94868
deruip <aronly1997@hotmail.com>
2017-07-27 15:31:15 -0400

    Added Planet abstract class

---
+++ AbstractPlanets/Planet.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+/*
+ * Name: Aron Ly
+ * Student #: 300932437
+ * Date: July 27, 2017
+ * Description: Abstract Planet superclass
+ * Version: 0.1 - Class created
+ */
+namespace AbstractPlanets
+{
+    public abstract class Planet
+    {
+        //private instance variables
+        private double _diameter;
+        private double _mass;
+        private int _moonCount;
+        private string _name;
+        private double _orbitalPeriod;
+        private int _ringCount;
+        private double _rotationPeriod;
+
+        //public properties
+        public double Diameter
+        {
+            get
+            {
+                return this._diameter;
+            }
+        }
+        public double Mass
+        {
+            get
+            {
+                return this._mass;
+            }
+        }
+        public int MoonCount
+        {
+            get
+            {
+                return this._moonCount;
+            }
+            set
+            {
+                this._moonCount = value;
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return this._name;
+            }
+        }
+        public double OrbitalPeriod
+        {
+            get
+            {
+                return this._orbitalPeriod;
+            }
+            set
+            {
+                this._orbitalPeriod = value;
+            }
+        }
+        public int RingCount
+        {
+            get
+            {
+                return this._ringCount;
+            }
+            set
+            {
+                this._ringCount = value;
+            }
+        }
+        public double RotationPeriod
+        {
+            get
+            {
+                return this._rotationPeriod;
+            }
+            set
+            {
+                this._rotationPeriod = value;
+            }
+        }
+
+        //constructors
+        public Planet(string name, double diameter, double mass)
+        {
+            _name = name;
+            _diameter = diameter;
+            _mass = mass;
+        }
+        //private methods
+
+        //public methods
+        public override string ToString()
+        {
+            return string.Format("Name: {0}\nDiameter: {1} - Mass: {2}", Name, Diameter, Mass);
+        }
+
+    }
+}
--- AbstractPlanets/Program.cs
+++ AbstractPlanets/Program.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/*
+ * Name: Aron Ly
+ * Student #: 300932437
+ * Date: July 27, 2017
+ * Description: Driver class
+ * Version: 0.1 - Class created
+ */
 namespace AbstractPlanets
 {
     class Program
     {
         static void Main(string[] args)
         {
-
+            
         }
     }
 }
